e8d6db7889fe6f77288fd89df41b0b6a8383c25e
github username <github email address>
2017-10-11 22:20:54 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,46 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
 
   end
+
+
 
   def count_sentences
+    count = 0
+    i = 0
+    variable = self + " "
 
+    while i < variable.length
+      if (variable[i] == "." && variable[i+1] == " ") || (variable[i] == "?" && variable[i+1] == " ")|| (variable[i] == "!" && variable[i+1] == " ")
+        count +=1
+      end
+      i += 1
+    end
+
+    count
   end
+
 end
